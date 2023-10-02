6d11cda7e9be806ae3be3bde1e6b216245c4c25c
Nick Lalic <nick.lalic@gmail.com>
2023-10-02 13:41:31 -0600

    ➕ added tool requirement cmake ~= 3.20.0

--- conanfile.py
+++ conanfile.py
@@ -5,31 +5,36 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 class RohdeSchwarzConan(ConanFile):
     name    = "rohdeschwarz"
     version = "1.0.0"
-
-
-    # Optional metadata
     license = "R&S Royalty Free License"
     author  = "Nick Lalic <nick.lalic@rsa.rohde-schwarz.com"
-    url     = ""
+    url     = "https://github.com/Terrabits/boost-tcp-client"
     description = "Boost TCP Socket example for Rohde & Schwarz instrument control"
     topics  = ("Rohde", "Schwarz", "R&S", "Boost", "tcp", "socket")
 
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
-    options  = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options  = {
+        "shared": [True, False],
+        "fPIC":   [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC":   True
+    }
 
 
+    # package type
     package_type = 'library'
 
 
-    # Sources are located in the same place as this recipe, copy them to the recipe
+    # sources
     exports_sources = "CMakeLists.txt", "src/*", "include/*"
 
 
-    def requirements(self):
-        self.requires('boost/1.83.0')
+    # requirements
+    requires      = 'boost/[>=1.83 <2.0]'
+    tool_requires = 'cmake/[>=3.27 <4.0]'
 
 
     def config_options(self):
