87e7bd5047735c3d6bbb0be00e243698a5544960
Nick Lalic <nick.lalic@gmail.com>
2023-10-03 18:03:43 -0600

    🐛 rohdeschwarz requires c++ standard >=17

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conan import ConanFile
+from conan.tools.build import check_min_cppstd
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 
@@ -46,6 +47,10 @@ class RohdeSchwarzConan(ConanFile):
     def config_options(self):
         if self.settings.os == "Windows":
             self.options.rm_safe("fPIC")
+
+
+    def validate(self):
+        check_min_cppstd(self, '17')
 
 
     def layout(self):
