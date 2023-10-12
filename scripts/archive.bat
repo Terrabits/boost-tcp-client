c26b7f93f10a9ec7b5ca44319b2179b2b5a92cbf
Nick Lalic <nick.lalic@gmail.com>
2023-10-12 13:49:07 -0600

    🔨 added archive script for source deployment.

---
+++ scripts/archive.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+SET "ROOT_DIR=%~dp0.."
+cd /d "%ROOT_DIR%"
+
+
+REM archive
+mkdir dist
+git archive --output dist\rohdeschwarz-src-0.1.0.zip main
--- scripts/clean.bat
+++ scripts/clean.bat
@@ -5,7 +5,8 @@ cd /d "%ROOT_DIR%"
 
 
 REM clean
-call scripts\examples\clean.bat
-call scripts\test_package\clean.bat
-rmdir /S /Q build
-del   /Q    CMakeUserPresets.json  >nul  2>&1
+call  scripts\examples\clean.bat
+call  scripts\test_package\clean.bat
+rmdir build  /S /Q
+rmdir dist   /S /Q
+del   CMakeUserPresets.json /Q >nul  2>&1
